5b48b7f538f86725360827d32520bb1bc82725df
jenniferlai43 <jenniferlai43@gmail.com>
2018-10-07 13:32:08 -0700

    Fixed gitignore

--- models/email.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-const Schema = mongoose.Schema;
-
-var emailSchema = new Schema({
-	email_address: String
-});
-
-module.exports = mongoose.model('email_address', emailSchema);
