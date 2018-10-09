9259b01e6272db9bc120ee5e7c2bf5795902f008
jenniferlai43 <jenniferlai43@gmail.com>
2018-10-08 22:11:18 -0700

    Finished heroku serverside, added css loader

--- public/email.js
+++ public/email.js
@@ -1,8 +1,28 @@
 $(document).ready(() => {
+	console.log("in ajax");
 
 	function resetForm(){
 		$('#email_input').val("");
 	}
+
+	function submitBack()
+      {
+        var check = $('#check')[0];
+        var submit_text = $('#submit_text')[0];
+        check.style.display = "none";
+        submit_text.style.display = "block";
+      }
+
+	function cssLoader()
+      {
+        var check = $('#check')[0];
+        var submit_text = $('#submit_text')[0];
+        check.style.display = "block";
+        submit_text.style.display = "none";
+        setTimeout(submitBack, 1900);
+      }
+
+     
 
 	$('form').on('submit', (e) =>{
 		e.preventDefault();
@@ -14,6 +34,7 @@ $(document).ready(() => {
 			data: email,
 			success: ((data)=>{
 				resetForm();
+				cssLoader();
 			})
 		});
 		
